c558d2916527cf13c3ab0f9449c82fce2cd826d9
Waris Gill <waris@vt.edu>
2024-12-09 23:38:41 -0500

    code is refactored

--- genfl/strategy.py
+++ genfl/strategy.py
@@ -3,7 +3,7 @@
 
 import flwr as fl
 from genfl.model import set_parameters
-from genfl.neuron_provenance import NeuronProvenance  # Add import
+from genfl.neuron_provenance import provenance_of_fl_clients  # Add import
 
 
 class FedAvgWithGenFL(fl.server.strategy.FedAvg):
@@ -21,9 +21,9 @@ class FedAvgWithGenFL(fl.server.strategy.FedAvg):
     def aggregate_fit(self, server_round, results, failures):
         """Aggregate clients updates."""
 
-        client2model = {fit_res.metrics["cid"]: self._to_pt_model(
-            fit_res.parameters) for _, fit_res in results}
-        c2nk = {fit_res.metrics["cid"]: fit_res.metrics.get("data_points", 0) for _, fit_res in results}
+        client2model = {fit_res.metrics["cid"]: self._to_pt_model(fit_res.parameters) for _, fit_res in results}
+        
+        # c2nk = {fit_res.metrics["cid"]: fit_res.metrics.get("data_points", 0) for _, fit_res in results}
 
         aggregated_parameters, aggregated_metrics = super(
         ).aggregate_fit(server_round, results, failures)
@@ -31,14 +31,8 @@ class FedAvgWithGenFL(fl.server.strategy.FedAvg):
 
         global_model = self._to_pt_model(aggregated_parameters)
 
-        self.provenance = NeuronProvenance(gmodel=global_model,
-            c2model=client2model,
-            c2nk=c2nk,
-            test_data=self.test_data
-        )  # Initialize NeuronProvenance
-        provenance_results = self.provenance.run()  # Compute provenance
-        aggregated_metrics['provenance'] = provenance_results  # Add to metrics
-
+        res  = provenance_of_fl_clients(gmodel=global_model, c2model=client2model, test_data=self.test_data)  
+        aggregated_metrics['provenance'] = res  # Add to metrics
         return aggregated_parameters, aggregated_metrics
 
     def _to_pt_model(self, parameters):
