d32d9053e6eecf0f8f538f35453a378735ef80de
Waris Gill <waris@vt.edu>
2024-12-22 22:34:29 -0500

    now fine tuning is working fine with packing. Fixed manually the end of text token

--- fine_tune.py
+++ fine_tune.py
@@ -9,7 +9,7 @@ from datasets import load_dataset
 from trl.trainer.utils import ConstantLengthDataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
-from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
+from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM, setup_chat_format
 
 from peft import LoraConfig, get_peft_model, PeftModel
 from peft.utils import prepare_model_for_kbit_training
@@ -24,23 +24,26 @@ random.seed(42)
 
 
 def _prompt(instruction, input_of_instruction):
-    # print(row)
-    prompt = f"Below is an instruction that describes a task. Write a response that appropriately completes the request. ### Instruction:\n{instruction}"
-
+    prompt = f"### Instruction:\n{instruction}"
     if len(input_of_instruction) > 2:
-        prompt += f"### Input:\n{input_of_instruction}"
-
-    prompt += ' \n\n### Response:\n'
-
+        prompt += f"\n### Input:\n{input_of_instruction}"
+    prompt += '\n### Response:\n'
     return prompt
 
-def create_alpaca_prompt_with_response(example, eos_token):
-    all_examples  = []
+
+def create_alpaca_prompt_with_response_collate(example, eos_token):
+    all_examples = []
     for i in range(len(example['instruction'])):
         instruct = _prompt(example['instruction'][i], example['input'][i])
-        prompt =  instruct + example["output"][i] +" "+  eos_token
+        prompt = instruct + example["output"][i] + " " + eos_token + " " 
         all_examples.append(prompt)
     return all_examples
+
+
+def create_alpaca_prompt_with_response(example, eos_token):
+    instruct = _prompt(example['instruction'], example['input'])
+    prompt = instruct + example["output"] + " " + eos_token + " " 
+    return prompt
 
 
 def load_datasets(dname="lucasmccabe-lmi/CodeAlpaca-20k"):
@@ -52,31 +55,35 @@ def load_datasets(dname="lucasmccabe-lmi/CodeAlpaca-20k"):
     print(f"Train: {len(train)}")
     print(f"Val: {len(val)}")
     print(f"Test: {len(test)}")
+
+    # train = train.select(range(4*1000))
+
+
     return train, val, test, ds
 
 
-def get_model_and_tokenizer(mname, special_tokens=None):
+def get_model_and_tokenizer(mname):
     # quantization_config = BitsAndBytesConfig(load_in_4bit=True)
-    tokenizer = AutoTokenizer.from_pretrained(mname)
+    tokenizer = AutoTokenizer.from_pretrained(
+        mname, use_fast=True)
     model = AutoModelForCausalLM.from_pretrained(
         mname, torch_dtype=torch.bfloat16)
 
-    if special_tokens is not None:
-        tokenizer.add_tokens(special_tokens)
-        model.resize_token_embeddings(len(tokenizer))
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.pad_token_id = tokenizer.eos_token_id
-
+    
     # model = prepare_model_for_kbit_training(model)
     peft_config = LoraConfig(
-        r=64,  # the rank of the LoRA matrices
-        lora_alpha=16,  # the weight
+        r=8,  # the rank of the LoRA matrices
+        lora_alpha=8,  # the weight
         lora_dropout=0.1,  # dropout to add to the LoRA layers
         task_type="CAUSAL_LM",
         # the name of the layers to add LoRA
         # target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+        # target_modules='all-linear',
     )
+    model, tokenizer =  setup_chat_format(model=model, tokenizer=tokenizer)
+
     model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
     return model, tokenizer
 
 
@@ -94,7 +101,7 @@ def generat_hf(model, tokenizer, terminators,  prompt, max_new_tokens=256):
     return text
 
 
-def _manual_generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, terminators=None):
+def _manual_generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, terminators=None, tokenizer=None):
     model.eval()
     model = model.cuda()
     idx = idx.cuda()
@@ -119,19 +126,22 @@ def _manual_generate(model, idx, max_new_tokens, context_size, temperature=0.0, 
 
         else:
             next_token_id = torch.argmax(logits, dim=-1, keepdim=True)
+        
+        
+        # token =  tokenizer.decode(next_token_id.item())
+        # print((token, next_token_id.item()))
 
-        # print(next_token_id.item(), eos_id)
+       
 
-        # print(f"Token: {next_token_id.item()}, terminator: {terminators}")  
+        temp_id = next_token_id.item()
 
-        if next_token_id.item() in terminators:
+        if temp_id in terminators:
+            print(terminators)
             print(
-                " =============================Found EOS token =============================")
-            exit(0)
+                f" =============================Found EOS token {temp_id} =============================")
+            # exit(0)
             break
-
         idx = torch.cat((idx, next_token_id), dim=-1)
-    # print(f'Tokens {idx}')
     return idx
 
 
@@ -140,11 +150,10 @@ def generate_self(model, tokenizer, terminators, prompt, max_new_tokens=256, con
     model = model.cuda().eval()
     with torch.no_grad():
         m_outs = _manual_generate(
-            model, encoding["input_ids"], max_new_tokens=max_new_tokens, context_size=context_size,  terminators=terminators)
-        # print(m_outs.shape)
+            model, encoding["input_ids"], max_new_tokens=max_new_tokens, context_size=context_size,  terminators=terminators, tokenizer=tokenizer)
         m_outs = m_outs.squeeze(0)
         response = m_outs[encoding["input_ids"].shape[-1]:]
-        # print(m_outs.shape)
+    # print(f"ids: {response}")   
     text = tokenizer.decode(response, skip_special_tokens=False)
     text = " ".join(text.split())
     print(f'Response (Manual):\n ***|||{text}|||***\n\n')
@@ -153,33 +162,27 @@ def generate_self(model, tokenizer, terminators, prompt, max_new_tokens=256, con
 
 def main():
     cache_dir = "save_model_tokenizer/cache"
-    eos_token = '<|waris_eos|>'
+    mname = 'gpt2'
 
     train, val, test, whole_ds = load_datasets(dname='yahma/alpaca-cleaned')
-    # for e in whole_ds:
-    #     single_input = create_alpaca_prompt(e)
-    #     break
 
-    model, tokenizer = get_model_and_tokenizer(
-        "gpt2", special_tokens=[eos_token])
-
-    terminators = [tokenizer.eos_token_id,
-                   tokenizer.convert_tokens_to_ids(eos_token)]
-
-    # generat_hf(model, tokenizer, terminators,  single_input)
-    # generate_self(model, tokenizer, terminators, single_input)
-
-    instruction_template = "Below is an instruction that describes a task. Write a response that appropriately completes the request. ### Instruction:"
-    response_template = "### Response:\n"
-    collator = DataCollatorForCompletionOnlyLM(
-        instruction_template=instruction_template, response_template=response_template, tokenizer=tokenizer, mlm=False)
+    model, tokenizer = get_model_and_tokenizer(mname)
     
-    
-    training_args = SFTConfig(output_dir="./temp",  learning_rate=5e-5, num_train_epochs=10, per_device_train_batch_size=16,
-                              do_eval=False, logging_steps=10, max_steps=-1, lr_scheduler_type='linear', packing=False, max_seq_length=1024, 
+   
+
+
+
+    instruction_template = "### Instruction:\n"
+    response_template = "\n### Response:\n"
+    # collator = DataCollatorForCompletionOnlyLM(
+    #     instruction_template=instruction_template, response_template=response_template, tokenizer=tokenizer, mlm=False)
+
+    training_args = SFTConfig(output_dir="./temp",  learning_rate=0.001, num_train_epochs=10, per_device_train_batch_size=16,
+                              do_eval=False, logging_steps=10, max_steps=-1, lr_scheduler_type='linear', packing=True, max_seq_length=1024,
                               )
 
-    
+
+    print(f"Tokenizer eos: {tokenizer.eos_token_id}, eos_token: {tokenizer.eos_token}")
 
     trainer = SFTTrainer(
         model,
@@ -187,28 +190,30 @@ def main():
         eval_dataset=val,
         args=training_args,
         formatting_func=partial(
-            create_alpaca_prompt_with_response, eos_token=eos_token),
-        data_collator = collator
-
+            create_alpaca_prompt_with_response, eos_token=tokenizer.eos_token),
+        # data_collator=collator
     )
     cache = Index(cache_dir)
 
-
     # trainer.train()
+
     # cache['model'] = model.cpu()
     # cache['tokenizer'] = tokenizer
 
-    model = cache['model'].eval().cuda()
+    # model = cache['model'].eval().cuda()
     # tokenizer = cache['tokenizer']
+
+    terminators = [tokenizer.eos_token_id, tokenizer.pad_token_id, 50256]
+    
 
     for e in whole_ds:
         prompt = _prompt(e['instruction'], e['input'])
+        print(" ---------------- Start ----------------")
         print(f"Prompt: {prompt}")
-        print(" ---------------- Response ----------------")
-        generat_hf(model, tokenizer, terminators,  prompt)
+        # generat_hf(model, tokenizer, terminators,  prompt)
         generate_self(model, tokenizer, terminators, prompt)
-        # response end
         print(" ---------------- End ----------------")
+        _ = input("Press Enter to continue")
 
 
 main()
