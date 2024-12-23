c9abf7af32589dddf9968ec56ca19b16a41cf49d
Waris Gill <waris@vt.edu>
2024-12-23 16:01:19 -0500

    local phi is working great

--- local_phi_train.py
+++ local_phi_train.py
@@ -1,19 +1,20 @@
 import sys
 import logging
 
-import datasets
+
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model
 import torch
-import transformers
+
 from trl import SFTTrainer, SFTConfig, setup_chat_format
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import hydra
 from functools import partial
 
 ################
-# Prompt Utils
+# Prompt
 ################
+
 
 def _prompt(instruction, input_of_instruction):
     prompt = f"### Instruction:\n{instruction}"
@@ -46,7 +47,7 @@ def load_datasets(dname):
 
     columns = list(train.features)
 
-    return {"train": train, "val": val, "test": test, "columns": columns}
+    return {"train": train, "val": val, "test": test, "columns": columns, 'ds': ds}
 
 
 ################
@@ -75,6 +76,57 @@ def get_model_and_tokenizer(model_cfg, peft_cfg):
     return model, tokenizer
 
 
+def _manual_generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, terminators=None, tokenizer=None):
+    model.eval()
+    model = model.cuda()
+    idx = idx.cuda()
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            outputs = model(idx_cond)
+            logits = outputs.logits
+
+        logits = logits[:, -1, :]  # last token is the prediction
+
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]  # how does it get min val?
+            logits = torch.where(
+                logits < min_val, torch.tensor(float('-inf')), logits)
+
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            next_token_id = torch.multinomial(probs, num_samples=1)
+
+        else:
+            next_token_id = torch.argmax(logits, dim=-1, keepdim=True)
+
+        temp_id = next_token_id.item()
+
+        if temp_id in terminators:
+            print(terminators)
+            print(
+                f" =============================Found EOS token {temp_id} =============================")
+            break
+        idx = torch.cat((idx, next_token_id), dim=-1)
+    return idx
+
+
+def generate_self(model, tokenizer, terminators, prompt, max_new_tokens=256, context_size=1024):
+    encoding = tokenizer(prompt, return_tensors="pt",).to('cuda')
+    model = model.cuda().eval()
+    with torch.no_grad():
+        m_outs = _manual_generate(
+            model, encoding["input_ids"], max_new_tokens=max_new_tokens, context_size=context_size,  terminators=terminators, tokenizer=tokenizer)
+        m_outs = m_outs.squeeze(0)
+        response = m_outs[encoding["input_ids"].shape[-1]:]
+    text = tokenizer.decode(response, skip_special_tokens=False)
+    text = " ".join(text.split())
+    print(f'Response (Manual):\n ***|||{text}|||***\n\n')
+    return text
+
+
 ################
 # Main
 ################
@@ -101,6 +153,17 @@ def main(cfg) -> None:
     train_result = trainer.train()
     metrics = train_result.metrics
     trainer.log_metrics("train", metrics)
+
+    terminators = [tokenizer.eos_token_id, tokenizer.pad_token_id, 50256]
+
+    for e in ds_dict['ds']:
+        prompt = _prompt(e['instruction'], e['input'])
+        print(" ---------------- Start ----------------")
+        print(f"Prompt: {prompt}")
+        # generat_hf(model, tokenizer, terminators,  prompt)
+        generate_self(model, tokenizer, terminators, prompt)
+        print(" ---------------- End ----------------")
+        _ = input("Press Enter to continue")
 
 
 main()
