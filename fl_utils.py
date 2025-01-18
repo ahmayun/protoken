1bc2877de1b07c6bac3c8fbbf010cb35afb5f557
user <email>
2025-01-18 13:40:39 -0500

    code is splitted

---
+++ fl_utils.py
@@ -0,0 +1,44 @@
+import random
+import numpy as np
+import torch
+
+
+################
+# Fixed Helper Functions
+################
+
+
+def set_exp_key(cfg):
+    """Set the experiment key."""
+
+    key = f"hello-genfl-1-"
+
+    print(f"Default Key: {key}")
+
+    # temp = input("Enter the experiment key to change: ")
+    # if temp != "":
+    #     key = temp
+    # print(f"Experiment Key: {key}")
+
+    return key
+
+
+def config_sim_resources(cfg):
+    """Configure the resources for the simulation."""
+    client_resources = {"num_cpus": cfg.client_resources.num_cpus}
+    if cfg.device == "cuda":
+        client_resources["num_gpus"] = cfg.client_resources.num_gpus
+
+    init_args = {"num_cpus": cfg.total_cpus, "num_gpus": cfg.total_gpus}
+    backend_config = {
+        "client_resources": client_resources,
+        "init_args": init_args,
+    }
+    return backend_config
+
+
+def seed_everything(seed=786):
+    """Seed everything."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
